a42e2abc989df2ef2a48d95a16d434ce839f1609
Vinicius <vinilima07@gmail.com>
2019-09-02 17:34:11 -0300

    correct files

--- transportProtocol/Program.cs
+++ transportProtocol/Program.cs
@@ -7,22 +7,21 @@ using System.IO;
 namespace transportProtocol{
     class Program{
         static void Main(string[] args){
-            Boolean isServer = true;
+            Boolean isServer = false;
             int choice = 3;
             Console.WriteLine("Pick your type connection: (1) TCP - (2) UDP - (3) EXIT");
             choice = Int32.Parse(Console.ReadLine());
             if(choice == 1){
                 TCP tcp = new TCP(isServer);
-                tcp.registerLog("---------Intialize TCP Process-------");
+                tcp.registerLog("Intialize TCP Process");
                 tcp.registerLog("exit TCP process");
             }else if(choice == 2){  
-                Console.WriteLine("--------UDP---------");
+                Console.WriteLine("UDP");
                 UDP udp = new UDP(isServer);
-                udp.registerLog("---------Intialize UDP Process-------");
+                udp.registerLog("Intialize UDP Process");
                 udp.registerLog("exit UDP process");
             }else{
                 Console.WriteLine("exit process...");
-                udp.registerLog("exit UDP process");
                 return;
             }
         }
@@ -35,18 +34,22 @@ namespace transportProtocol{
         }
 
         public Boolean receive(){
-            int port = 0666;
-            String ipAddress = "172.16.253.72";
-            byte[] bytes = new Byte[1024];2
+           
+            try {
+                int port = 11111;
+                byte[] bytes = new Byte[1024];
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName()); 
+                IPAddress ipAddr = ipHost.AddressList[0]; 
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111); 
             
-            try { 
-                Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp); 
-                socket.Bind(new IPEndPoint(ipAddress, port)); //associa um endereco a network do server
-                socket.Listen();    //coloca o servidor em estado de recebimento de conexoes
+                // Creation TCP/IP Socket using  
+                // Socket Class Costructor 
+                Socket socket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp); 
+                socket.Bind(new IPEndPoint(ipAddr, port)); //associa um endereco a network do server
+                socket.Listen(10);    //coloca o servidor em estado de recebimento de conexoes - 10 tamanho da fila
 
-                while (true) { 
-                    
-                    Console.WriteLine("Waiting connection ... "); 
+                while (true) {                     
+                    Console.WriteLine("Waiting connection ... ");
                     Socket clientSocket = socket.Accept(); //recebe a conexao do cliente
                     int numByte = clientSocket.Receive(bytes); 
                     String data = Encoding.ASCII.GetString(bytes, 0, numByte); //show PDU
@@ -59,13 +62,14 @@ namespace transportProtocol{
 
                     clientSocket.Shutdown(SocketShutdown.Both);
                     clientSocket.Close(); //fecha a conexao com o cliente
+
                 } 
                 return true;
-            } 
-            
-            catch (Exception e) { 
-                Console.WriteLine(e.ToString()); 
-            } 
+            }catch (ArgumentNullException e){
+                Console.WriteLine("Argument Exception: {0}", e);
+            }catch (SocketException e){
+                Console.WriteLine("SocketException: {0}", e);
+            }   
 
             return false;
         }
@@ -75,12 +79,12 @@ namespace transportProtocol{
                 //StreamReader rd = new StreamReader(@'data');
                 byte[] msgSent = Encoding.ASCII.GetBytes("Test Client<EOF>"); 
                 byte[] msgReceiv = new byte[1024];
-                int port = 0666;
-                String ipAddress = "172.16.253.72";
-                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                int port = 11111;
+                String ipAddress = "192.168.0.5";
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IPv4);
                 
                 //conecta com o servidor
-                socket.Connect(new IPEndPoint(ipAddress, port));
+                socket.Connect(ipAddress, port);
 
                 if (!socket.Connected){
                     Console.WriteLine("unable to connect");
@@ -90,14 +94,14 @@ namespace transportProtocol{
                 }
                 
                 socket.Send(msgSent); 
-                registerLog("Send message to "+sender.RemoteEndPoint.ToString());
+                registerLog("Send message to "+socket.RemoteEndPoint.ToString());
 
                 //resposta do servidor
                 socket.Receive(msgReceiv);
                 registerLog("Receive server answer");
 
-                int byteRecv = socket.Receive(msgReceived); 
-                Console.WriteLine(Encoding.ASCII.GetString(msgReceived, 0, byteRecv)); 
+                int byteRecv = socket.Receive(msgReceiv); 
+                Console.WriteLine(Encoding.ASCII.GetString(msgReceiv, 0, byteRecv)); 
 
                 socket.Close();
 
@@ -125,11 +129,10 @@ namespace transportProtocol{
             wr.WriteLine(msg);
             wr.Close();
         }
-
     }
 
     class UDP{
-        public UDP(){
+        public UDP(Boolean isServer){
 
         }
 
