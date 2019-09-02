b81f47beaec027dacd1f12061662b8951b70809e
Vinicius <vinilima07@gmail.com>
2019-09-02 19:25:30 -0300

    update files

--- transportProtocol/Program.cs
+++ transportProtocol/Program.cs
@@ -7,7 +7,7 @@ using System.IO;
 namespace transportProtocol{
     class Program{
         static void Main(string[] args){
-            Boolean isServer = false;
+            Boolean isServer = true;
             int choice = 3;
             Console.WriteLine("Pick your type connection: (1) TCP - (2) UDP - (3) EXIT");
             choice = Int32.Parse(Console.ReadLine());
@@ -40,7 +40,7 @@ namespace transportProtocol{
                 byte[] bytes = new Byte[1024];
                 IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName()); 
                 IPAddress ipAddr = ipHost.AddressList[0]; 
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111); 
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port); 
             
                 // Creation TCP/IP Socket using  
                 // Socket Class Costructor 
@@ -54,7 +54,7 @@ namespace transportProtocol{
                     int numByte = clientSocket.Receive(bytes); 
                     String data = Encoding.ASCII.GetString(bytes, 0, numByte); //show PDU
                 
-                    Console.WriteLine("Data receive from"); 
+                    Console.WriteLine("Data receive from");
                     byte[] msgAnswer = Encoding.ASCII.GetBytes("Data receive"); 
 
                     //responde o client 
@@ -81,9 +81,7 @@ namespace transportProtocol{
                 byte[] msgReceiv = new byte[1024];
                 int port = 11111;
                 String ipAddress = "192.168.0.5";
-                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IPv4);
-                
-                //conecta com o servidor
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 socket.Connect(ipAddress, port);
 
                 if (!socket.Connected){
@@ -133,14 +131,74 @@ namespace transportProtocol{
 
     class UDP{
         public UDP(Boolean isServer){
-
+            if(isServer) receive();
+            else send();
         }
 
         public Boolean receive(){
+            int port = 11111;
+            byte[] bytes = new Byte[1024];
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName()); 
+            IPAddress ipAddr = ipHost.AddressList[0]; 
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+            EndPoint endPoint = (EndPoint)localEndPoint;
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            Object o = new Object();
+
+            try{
+                socket.BeginReceiveFrom(bytes, 0, bytes.Length, SocketFlags.None, ref endPoint, (ar) =>
+                {
+                    socket = (Socket)ar.AsyncState;
+                    int byteRecv = socket.EndSend(ar);
+                    
+                }, o);
+
+            }catch (ArgumentNullException e){
+                Console.WriteLine("Argument Exception: {0}", e);
+            }catch (SocketException e){
+                Console.WriteLine("SocketException: {0}", e);
+            }   
             return false;
         }
 
         public Boolean send(){
+            try{
+                //StreamReader rd = new StreamReader(@'data');
+                byte[] msgSent = Encoding.ASCII.GetBytes("Test Client<EOF>"); 
+                byte[] msgReceiv = new byte[1024];
+                int port = 11111;
+                String ipAddress = "192.168.0.5";
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                
+                //conecta com o servidor
+                socket.Connect(ipAddress, port);
+
+                if (!socket.Connected){
+                    Console.WriteLine("unable to connect");
+                    return false;
+                }else{
+                    registerLog("Socket connected to -> {0} "+socket.RemoteEndPoint.ToString());                
+                }
+                
+                socket.Send(msgSent); 
+                registerLog("Send message to "+socket.RemoteEndPoint.ToString());
+
+                //resposta do servidor
+                socket.Receive(msgReceiv);
+                registerLog("Receive server answer");
+
+                int byteRecv = socket.Receive(msgReceiv); 
+                Console.WriteLine(Encoding.ASCII.GetString(msgReceiv, 0, byteRecv)); 
+
+                socket.Close();
+
+                return true;
+            }catch (ArgumentNullException e){
+                Console.WriteLine("Argument Exception: {0}", e);
+            }catch (SocketException e){
+                Console.WriteLine("SocketException: {0}", e);
+            }    
             return false;
         }
 
